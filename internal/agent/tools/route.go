A-i-developement/fastclaw2

fix(tools): block FastClaw internals from host file routing

internal/agent/tools/route.go
@@ -98,10 +98,11 @@ func (r *Registry) routeFor(path string, op Operation) RouteTarget {
 	// Rule 2: local with sandbox configured → sandbox-first. Host disk is
 	// reachable only via explicit host-scope paths (the operator's
 	// Documents, an absolute /Users/<u>/... that's clearly NOT
-	// sandbox-internal, fastclaw-internal subtrees for upgrade ops).
+	// sandbox-internal). FastClaw internals must not be exposed through
+	// chat-facing file tools; operator maintenance should use host_exec.
 	if sandboxOK {
 		if isFastClawInternalPath(path) {
-			return RouteHostFS
+			return RouteSandbox
 		}
 		if isExplicitHostScope(path) {
 			return RouteHostFS
@@ -173,9 +174,8 @@ func isFastClawInternalPath(path string) bool {
 		return true
 	}
 	if filepath.IsAbs(path) {
-		if home, err := os.UserHomeDir(); err == nil {
-			fastclawDir := filepath.Join(home, ".fastclaw")
-			if path == fastclawDir || strings.HasPrefix(path, fastclawDir+string(filepath.Separator)) {
+		for _, root := range fastClawInternalRoots() {
+			if path == root || strings.HasPrefix(path, root+string(filepath.Separator)) {
 				return true
 			}
 		}
@@ -183,12 +183,24 @@ func isFastClawInternalPath(path string) bool {
 	return false
 }
 
+func fastClawInternalRoots() []string {
+	roots := []string{}
+	if h := os.Getenv("FASTCLAW_HOME"); h != "" {
+		roots = append(roots, filepath.Clean(h))
+	}
+	roots = append(roots, filepath.Clean("/root/.fastclaw"))
+	if home, err := os.UserHomeDir(); err == nil {
+		roots = append(roots, filepath.Join(home, ".fastclaw"))
+	}
+	return roots
+}
+
 // isSandboxOnlyPath reports whether path only exists inside the sandbox
 // container — typically a bind-mount target. Host has the bind-source at
 // a different location, so naive host expansion would always 404.
 //
 //   - ~/.agents/...    : npx skills' install dir (bind-mounted from
-//                        ~/.fastclaw/users/<uid>/skills/)
+//     ~/.fastclaw/users/<uid>/skills/)
 //   - /root/.agents/.. : same, via the sandbox-resolved absolute path
 func isSandboxOnlyPath(path string) bool {
 	if strings.HasPrefix(path, "~/.agents") || strings.HasPrefix(path, "/root/.agents") {

internal/agent/tools/route_test.go
@@ -1,0 +1,48 @@
+package tools
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+type fakeExecutor struct{}
+
+func (fakeExecutor) Exec(context.Context, string, time.Duration) (string, error) { return "", nil }
+func (fakeExecutor) ReadFile(context.Context, string) (string, error)            { return "", nil }
+func (fakeExecutor) WriteFile(context.Context, string, string) (string, error)   { return "", nil }
+func (fakeExecutor) ListDir(context.Context, string) (string, error)             { return "", nil }
+func (fakeExecutor) Backend() string                                             { return "test" }
+func (fakeExecutor) Close() error                                                { return nil }
+
+func TestFastClawInternalPathsIncludeRootAndEnvHome(t *testing.T) {
+	t.Setenv("FASTCLAW_HOME", "/srv/fastclaw")
+
+	cases := []string{
+		"~/.fastclaw",
+		"~/.fastclaw/workspaces",
+		"/root/.fastclaw",
+		"/root/.fastclaw/workspaces",
+		"/srv/fastclaw",
+		"/srv/fastclaw/workspaces",
+	}
+	for _, path := range cases {
+		if !isFastClawInternalPath(path) {
+			t.Fatalf("isFastClawInternalPath(%q) = false, want true", path)
+		}
+		if got, ok := hostHomePath(path); ok || got != "" {
+			t.Fatalf("hostHomePath(%q) = (%q, %v), want denied", path, got, ok)
+		}
+	}
+}
+
+func TestRouteForDoesNotExposeFastClawInternalsViaHostFS(t *testing.T) {
+	r := &Registry{executor: fakeExecutor{}}
+
+	if got := r.routeFor("/root/.fastclaw/workspaces", OpList); got != RouteSandbox {
+		t.Fatalf("routeFor FastClaw internal path = %v, want RouteSandbox", got)
+	}
+	if got := r.routeFor("/Users/maxwell/Documents/report.txt", OpRead); got != RouteHostFS {
+		t.Fatalf("routeFor explicit host document = %v, want RouteHostFS", got)
+	}
+}

tokenaissance/fastclaw

internal/agent/tools/route.go
@@ -98,10 +98,11 @@ func (r *Registry) routeFor(path string, op Operation) RouteTarget {
 	// Rule 2: local with sandbox configured → sandbox-first. Host disk is
 	// reachable only via explicit host-scope paths (the operator's
 	// Documents, an absolute /Users/<u>/... that's clearly NOT
-	// sandbox-internal, fastagent-internal subtrees for upgrade ops).
+	// sandbox-internal). FastAgent internals must not be exposed through
+	// chat-facing file tools; operator maintenance should use host_exec.
 	if sandboxOK {
 		if isFastAgentInternalPath(path) {
-			return RouteHostFS
+			return RouteSandbox
 		}
 		if isExplicitHostScope(path) {
 			return RouteHostFS
@@ -173,9 +174,8 @@ func isFastAgentInternalPath(path string) bool {
 		return true
 	}
 	if filepath.IsAbs(path) {
-		if home, err := os.UserHomeDir(); err == nil {
-			fastagentDir := filepath.Join(home, ".fastagent")
-			if path == fastagentDir || strings.HasPrefix(path, fastagentDir+string(filepath.Separator)) {
+		for _, root := range fastAgentInternalRoots() {
+			if path == root || strings.HasPrefix(path, root+string(filepath.Separator)) {
 				return true
 			}
 		}
@@ -183,12 +183,29 @@ func isFastAgentInternalPath(path string) bool {
 	return false
 }
 
+// fastAgentInternalRoots returns every location FastAgent manages at
+// runtime (install home, the sandbox-resolved /root path, and the
+// operator's home dir). File-tool routing refuses to treat these as
+// chat-accessible host paths — operator maintenance should use
+// host_exec instead.
+func fastAgentInternalRoots() []string {
+	roots := []string{}
+	if h := os.Getenv("FASTAGENT_HOME"); h != "" {
+		roots = append(roots, filepath.Clean(h))
+	}
+	roots = append(roots, filepath.Clean("/root/.fastagent"))
+	if home, err := os.UserHomeDir(); err == nil {
+		roots = append(roots, filepath.Join(home, ".fastagent"))
+	}
+	return roots
+}
+
 // isSandboxOnlyPath reports whether path only exists inside the sandbox
 // container — typically a bind-mount target. Host has the bind-source at
 // a different location, so naive host expansion would always 404.
 //
 //   - ~/.agents/...    : npx skills' install dir (bind-mounted from
-//                        ~/.fastagent/users/<uid>/skills/)
+//     ~/.fastagent/users/<uid>/skills/)
 //   - /root/.agents/.. : same, via the sandbox-resolved absolute path
 func isSandboxOnlyPath(path string) bool {
 	if strings.HasPrefix(path, "~/.agents") || strings.HasPrefix(path, "/root/.agents") {

internal/agent/tools/route_test.go
@@ -1,0 +1,48 @@
+package tools
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+type fakeExecutor struct{}
+
+func (fakeExecutor) Exec(context.Context, string, time.Duration) (string, error) { return "", nil }
+func (fakeExecutor) ReadFile(context.Context, string) (string, error)            { return "", nil }
+func (fakeExecutor) WriteFile(context.Context, string, string) (string, error)   { return "", nil }
+func (fakeExecutor) ListDir(context.Context, string) (string, error)             { return "", nil }
+func (fakeExecutor) Backend() string                                             { return "test" }
+func (fakeExecutor) Close() error                                                { return nil }
+
+func TestFastAgentInternalPathsIncludeRootAndEnvHome(t *testing.T) {
+	t.Setenv("FASTAGENT_HOME", "/srv/fastagent")
+
+	cases := []string{
+		"~/.fastagent",
+		"~/.fastagent/workspaces",
+		"/root/.fastagent",
+		"/root/.fastagent/workspaces",
+		"/srv/fastagent",
+		"/srv/fastagent/workspaces",
+	}
+	for _, path := range cases {
+		if !isFastAgentInternalPath(path) {
+			t.Fatalf("isFastAgentInternalPath(%q) = false, want true", path)
+		}
+		if got, ok := hostHomePath(path); ok || got != "" {
+			t.Fatalf("hostHomePath(%q) = (%q, %v), want denied", path, got, ok)
+		}
+	}
+}
+
+func TestRouteForDoesNotExposeFastAgentInternalsViaHostFS(t *testing.T) {
+	r := &Registry{executor: fakeExecutor{}}
+
+	if got := r.routeFor("/root/.fastagent/workspaces", OpList); got != RouteSandbox {
+		t.Fatalf("routeFor FastAgent internal path = %v, want RouteSandbox", got)
+	}
+	if got := r.routeFor("/Users/maxwell/Documents/report.txt", OpRead); got != RouteHostFS {
+		t.Fatalf("routeFor explicit host document = %v, want RouteHostFS", got)
+	}
+}