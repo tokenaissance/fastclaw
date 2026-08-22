A-i-developement/fastclaw2

Add Redis coordination and admin CLI coverage

internal/config/env.go
@@ -17,6 +17,7 @@ type EnvConfig struct {
 	Gateway EnvGateway
 	Storage EnvStorage
 	Sandbox EnvSandbox
+	Redis   EnvRedis
 	Log     EnvLog
 }
 
@@ -29,6 +30,15 @@ type EnvStorage struct {
 	Type        string // FASTCLAW_STORAGE_TYPE  — "sqlite" (default) or "postgres"
 	DSN         string // FASTCLAW_STORAGE_DSN   — empty = sqlite at $FASTCLAW_HOME/fastclaw.db
 	AutoMigrate bool   // FASTCLAW_STORAGE_AUTO_MIGRATE — default true
+}
+
+type EnvRedis struct {
+	Enabled  bool   // FASTCLAW_REDIS_ENABLED — enable Redis-backed bus + leases
+	Addr     string // FASTCLAW_REDIS_ADDR    — host:port, e.g. redis:6379
+	Username string // FASTCLAW_REDIS_USERNAME
+	Password string // FASTCLAW_REDIS_PASSWORD
+	DB       int    // FASTCLAW_REDIS_DB
+	Prefix   string // FASTCLAW_REDIS_PREFIX  — key prefix, default "fastclaw"
 }
 
 type EnvSandbox struct {
@@ -74,6 +84,28 @@ func LoadEnv() *EnvConfig {
 	}
 	if v := os.Getenv("FASTCLAW_STORAGE_AUTO_MIGRATE"); v != "" {
 		cfg.Storage.AutoMigrate = v == "true" || v == "1"
+	}
+
+	if v := os.Getenv("FASTCLAW_REDIS_ENABLED"); v != "" {
+		cfg.Redis.Enabled = v == "true" || v == "1"
+	}
+	if v := os.Getenv("FASTCLAW_REDIS_ADDR"); v != "" {
+		cfg.Redis.Addr = v
+		cfg.Redis.Enabled = true
+	}
+	if v := os.Getenv("FASTCLAW_REDIS_USERNAME"); v != "" {
+		cfg.Redis.Username = v
+	}
+	if v := os.Getenv("FASTCLAW_REDIS_PASSWORD"); v != "" {
+		cfg.Redis.Password = v
+	}
+	if v := os.Getenv("FASTCLAW_REDIS_DB"); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			cfg.Redis.DB = n
+		}
+	}
+	if v := os.Getenv("FASTCLAW_REDIS_PREFIX"); v != "" {
+		cfg.Redis.Prefix = v
 	}
 
 	if v := os.Getenv("FASTCLAW_SANDBOX_ENABLED"); v != "" {
@@ -190,6 +222,7 @@ func ScrubBootSecrets() {
 		"FASTCLAW_OBJECT_STORE_ENDPOINT",
 		"FASTCLAW_OBJECT_STORE_USESSL",
 		"FASTCLAW_OBJECT_STORE_ALIYUN_INTERNAL",
+		"FASTCLAW_REDIS_PASSWORD",
 		"BOXLITE_API_KEY",
 		"E2B_API_KEY",
 	}

internal/gateway/gateway.go
@@ -29,6 +29,7 @@ import (
 	"github.com/fastclaw-ai/fastclaw/internal/config"
 	"github.com/fastclaw-ai/fastclaw/internal/cron"
 	"github.com/fastclaw-ai/fastclaw/internal/plugin"
+	"github.com/fastclaw-ai/fastclaw/internal/rediscoord"
 	coderuntime "github.com/fastclaw-ai/fastclaw/internal/runtime"
 	"github.com/fastclaw-ai/fastclaw/internal/sandbox"
 	"github.com/fastclaw-ai/fastclaw/internal/scope"
@@ -43,6 +44,7 @@ import (
 	"github.com/fastclaw-ai/fastclaw/internal/users"
 	"github.com/fastclaw-ai/fastclaw/internal/webhook"
 	"github.com/fastclaw-ai/fastclaw/internal/workspace"
+	"github.com/redis/go-redis/v9"
 )
 
 var toolProviderRegistry = func() *toolproviders.Registry {
@@ -250,7 +252,32 @@ func New(env *config.EnvConfig) (*Gateway, error) {
 	if env == nil {
 		env = &config.EnvConfig{}
 	}
+	holderID := uuid.NewString()
+	slog.Info("gateway holder id", "id", holderID)
+
+	var redisClient *redis.Client
 	mb := bus.New()
+	if env.Redis.Enabled {
+		addr := strings.TrimSpace(env.Redis.Addr)
+		if addr == "" {
+			addr = "127.0.0.1:6379"
+		}
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Username: env.Redis.Username,
+			Password: env.Redis.Password,
+			DB:       env.Redis.DB,
+		})
+		if err := redisClient.Ping(context.Background()).Err(); err != nil {
+			return nil, fmt.Errorf("connect redis: %w", err)
+		}
+		mb = bus.NewRedis(bus.RedisConfig{
+			Client:   redisClient,
+			Prefix:   redisPrefix(env.Redis.Prefix),
+			Group:    "fastclaw-gateway",
+			Consumer: holderID,
+		})
+	}
 
 	homeDir, _ := config.HomeDir()
 	st, err := store.New(&store.StorageConfig{
@@ -301,15 +328,16 @@ func New(env *config.EnvConfig) (*Gateway, error) {
 	}
 	ws := wsInner
 
-	// holderID is the per-process identifier stamped into
-	// channel_leases.holder_id. Stable for the lifetime of this
-	// gateway so renewals keep matching the row; a peer process
-	// generates its own and can only steal the lease once ours
-	// expires. Logged at boot so ops can correlate "who is currently
-	// driving this WeChat bot" with a specific replica.
-	holderID := uuid.NewString()
-	slog.Info("gateway holder id", "id", holderID)
-	chanMgr := channels.NewManagerWithLeaser(mb, storeLeaser{st: st}, holderID)
+	// holderID is the per-process identifier used by the cross-replica
+	// channel lease. Redis is preferred when configured; otherwise the
+	// historical DB-backed lease keeps single-instance / no-Redis deploys
+	// working unchanged.
+	var leaser channels.Leaser = storeLeaser{st: st}
+	if redisClient != nil {
+		leaser = rediscoord.NewLeaser(redisClient, redisPrefix(env.Redis.Prefix))
+		slog.Info("redis channel leaser enabled", "prefix", redisPrefix(env.Redis.Prefix))
+	}
+	chanMgr := channels.NewManagerWithLeaser(mb, leaser, holderID)
 	// Always-on web channel: routes cron-fired (and any other
 	// async-emitted) outbound messages to the dashboard's SSE
 	// subscribers so the user sees the agent's reply live instead of
@@ -584,15 +612,19 @@ func (g *Gateway) Run() error {
 			case <-ctx.Done():
 				return
 			case <-reloadCh:
-				slog.Info("received reload signal, reloading agents")
+				slog.Info("received reload signal, reloading agents and scheduled jobs")
 				if err := g.ReloadAgents(); err != nil {
 					slog.Warn("agent reload failed", "error", err)
 				}
+				cron.NotifyJobCreated()
 			}
 		}
 	}()
 
 	var wg sync.WaitGroup
+	if err := g.bus.Start(ctx); err != nil {
+		return fmt.Errorf("start message bus: %w", err)
+	}
 	wg.Add(1)
 	go func() { defer wg.Done(); g.users.startEvictor(ctx) }()
 	wg.Add(1)
@@ -677,6 +709,14 @@ func makeStoreFirstAgentFileLoader(st store.Store) func(string, string) (config.
 func defaultStr(v, fallback string) string {
 	if v == "" {
 		return fallback
+	}
+	return v
+}
+
+func redisPrefix(v string) string {
+	v = strings.Trim(v, ":")
+	if v == "" {
+		return "fastclaw"
 	}
 	return v
 }
@@ -795,7 +835,7 @@ func registerChannelsFromStore(st store.Store, mb *bus.MessageBus, chanMgr *chan
 }
 
 // allChannelRows returns every channel row regardless of ownership —
-// system rows ('','') plus per-user, per-agent, and per-(user, agent)
+// system rows ("", "") plus per-user, per-agent, and per-(user, agent)
 // rows. The boot path needs the union so each owner's adapter is
 // hot-started; per-row routing is decided later at message-receipt
 // time via LookupChannelByCredential.

internal/rediscoord/lease.go
@@ -1,0 +1,82 @@
+package rediscoord
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// Leaser implements channels.Leaser with Redis keys. Each lease is a single
+// key whose value is the current gateway holder ID and whose TTL is the
+// freshness window.
+type Leaser struct {
+	client *redis.Client
+	prefix string
+}
+
+func NewLeaser(client *redis.Client, prefix string) *Leaser {
+	prefix = strings.Trim(prefix, ":")
+	if prefix == "" {
+		prefix = "fastclaw"
+	}
+	return &Leaser{client: client, prefix: prefix}
+}
+
+func (l *Leaser) Acquire(ctx context.Context, channel, accountID, holderID string, ttl time.Duration) (bool, error) {
+	key := l.key(channel, accountID)
+	ok, err := l.client.SetNX(ctx, key, holderID, ttl).Result()
+	if err != nil {
+		return false, err
+	}
+	if ok {
+		return true, nil
+	}
+	current, err := l.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	if current != holderID {
+		return false, nil
+	}
+	return l.Renew(ctx, channel, accountID, holderID, ttl)
+}
+
+func (l *Leaser) Renew(ctx context.Context, channel, accountID, holderID string, ttl time.Duration) (bool, error) {
+	res, err := renewScript.Run(ctx, l.client, []string{l.key(channel, accountID)}, holderID, int64(ttl/time.Millisecond)).Int()
+	if err != nil {
+		return false, err
+	}
+	return res == 1, nil
+}
+
+func (l *Leaser) Release(ctx context.Context, channel, accountID, holderID string) error {
+	_, err := releaseScript.Run(ctx, l.client, []string{l.key(channel, accountID)}, holderID).Int()
+	return err
+}
+
+func (l *Leaser) key(channel, accountID string) string {
+	if accountID == "" {
+		accountID = "default"
+	}
+	return fmt.Sprintf("%s:lease:channel:%s:%s", l.prefix, channel, accountID)
+}
+
+var renewScript = redis.NewScript(`
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+  return redis.call("PEXPIRE", KEYS[1], ARGV[2])
+end
+return 0
+`)
+
+var releaseScript = redis.NewScript(`
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+  return redis.call("DEL", KEYS[1])
+end
+return 0
+`)

tokenaissance/fastclaw

internal/config/env.go
@@ -17,6 +17,7 @@ type EnvConfig struct {
 	Gateway EnvGateway
 	Storage EnvStorage
 	Sandbox EnvSandbox
+	Redis   EnvRedis
 	Log     EnvLog
 }
 
@@ -29,6 +30,15 @@ type EnvStorage struct {
 	Type        string // FASTAGENT_STORAGE_TYPE  — "sqlite" (default) or "postgres"
 	DSN         string // FASTAGENT_STORAGE_DSN   — empty = sqlite at $FASTAGENT_HOME/fastagent.db
 	AutoMigrate bool   // FASTAGENT_STORAGE_AUTO_MIGRATE — default true
+}
+
+type EnvRedis struct {
+	Enabled  bool   // FASTAGENT_REDIS_ENABLED — enable Redis-backed bus + leases
+	Addr     string // FASTAGENT_REDIS_ADDR    — host:port, e.g. redis:6379
+	Username string // FASTAGENT_REDIS_USERNAME
+	Password string // FASTAGENT_REDIS_PASSWORD
+	DB       int    // FASTAGENT_REDIS_DB
+	Prefix   string // FASTAGENT_REDIS_PREFIX  — key prefix, default "fastagent"
 }
 
 type EnvSandbox struct {
@@ -74,6 +84,28 @@ func LoadEnv() *EnvConfig {
 	}
 	if v := os.Getenv("FASTAGENT_STORAGE_AUTO_MIGRATE"); v != "" {
 		cfg.Storage.AutoMigrate = v == "true" || v == "1"
+	}
+
+	if v := os.Getenv("FASTAGENT_REDIS_ENABLED"); v != "" {
+		cfg.Redis.Enabled = v == "true" || v == "1"
+	}
+	if v := os.Getenv("FASTAGENT_REDIS_ADDR"); v != "" {
+		cfg.Redis.Addr = v
+		cfg.Redis.Enabled = true
+	}
+	if v := os.Getenv("FASTAGENT_REDIS_USERNAME"); v != "" {
+		cfg.Redis.Username = v
+	}
+	if v := os.Getenv("FASTAGENT_REDIS_PASSWORD"); v != "" {
+		cfg.Redis.Password = v
+	}
+	if v := os.Getenv("FASTAGENT_REDIS_DB"); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			cfg.Redis.DB = n
+		}
+	}
+	if v := os.Getenv("FASTAGENT_REDIS_PREFIX"); v != "" {
+		cfg.Redis.Prefix = v
 	}
 
 	if v := os.Getenv("FASTAGENT_SANDBOX_ENABLED"); v != "" {
@@ -190,6 +222,7 @@ func ScrubBootSecrets() {
 		"FASTAGENT_OBJECT_STORE_ENDPOINT",
 		"FASTAGENT_OBJECT_STORE_USESSL",
 		"FASTAGENT_OBJECT_STORE_ALIYUN_INTERNAL",
+		"FASTAGENT_REDIS_PASSWORD",
 		"BOXLITE_API_KEY",
 		"E2B_API_KEY",
 	}

internal/gateway/gateway.go
@@ -29,6 +29,7 @@ import (
 	"github.com/fastclaw-ai/fastclaw/internal/config"
 	"github.com/fastclaw-ai/fastclaw/internal/cron"
 	"github.com/fastclaw-ai/fastclaw/internal/plugin"
+	"github.com/fastclaw-ai/fastclaw/internal/rediscoord"
 	coderuntime "github.com/fastclaw-ai/fastclaw/internal/runtime"
 	"github.com/fastclaw-ai/fastclaw/internal/sandbox"
 	"github.com/fastclaw-ai/fastclaw/internal/scope"
@@ -43,6 +44,7 @@ import (
 	"github.com/fastclaw-ai/fastclaw/internal/users"
 	"github.com/fastclaw-ai/fastclaw/internal/webhook"
 	"github.com/fastclaw-ai/fastclaw/internal/workspace"
+	"github.com/redis/go-redis/v9"
 )
 
 var toolProviderRegistry = func() *toolproviders.Registry {
@@ -250,7 +252,32 @@ func New(env *config.EnvConfig) (*Gateway, error) {
 	if env == nil {
 		env = &config.EnvConfig{}
 	}
+	holderID := uuid.NewString()
+	slog.Info("gateway holder id", "id", holderID)
+
+	var redisClient *redis.Client
 	mb := bus.New()
+	if env.Redis.Enabled {
+		addr := strings.TrimSpace(env.Redis.Addr)
+		if addr == "" {
+			addr = "127.0.0.1:6379"
+		}
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Username: env.Redis.Username,
+			Password: env.Redis.Password,
+			DB:       env.Redis.DB,
+		})
+		if err := redisClient.Ping(context.Background()).Err(); err != nil {
+			return nil, fmt.Errorf("connect redis: %w", err)
+		}
+		mb = bus.NewRedis(bus.RedisConfig{
+			Client:   redisClient,
+			Prefix:   redisPrefix(env.Redis.Prefix),
+			Group:    "fastagent-gateway",
+			Consumer: holderID,
+		})
+	}
 
 	homeDir, _ := config.HomeDir()
 	st, err := store.New(&store.StorageConfig{
@@ -321,15 +348,16 @@ func New(env *config.EnvConfig) (*Gateway, error) {
 
 	ws := wsInner
 
-	// holderID is the per-process identifier stamped into
-	// channel_leases.holder_id. Stable for the lifetime of this
-	// gateway so renewals keep matching the row; a peer process
-	// generates its own and can only steal the lease once ours
-	// expires. Logged at boot so ops can correlate "who is currently
-	// driving this WeChat bot" with a specific replica.
-	holderID := uuid.NewString()
-	slog.Info("gateway holder id", "id", holderID)
-	chanMgr := channels.NewManagerWithLeaser(mb, storeLeaser{st: st}, holderID)
+	// holderID is the per-process identifier used by the cross-replica
+	// channel lease. Redis is preferred when configured; otherwise the
+	// historical DB-backed lease keeps single-instance / no-Redis deploys
+	// working unchanged.
+	var leaser channels.Leaser = storeLeaser{st: st}
+	if redisClient != nil {
+		leaser = rediscoord.NewLeaser(redisClient, redisPrefix(env.Redis.Prefix))
+		slog.Info("redis channel leaser enabled", "prefix", redisPrefix(env.Redis.Prefix))
+	}
+	chanMgr := channels.NewManagerWithLeaser(mb, leaser, holderID)
 	// Always-on web channel: routes cron-fired (and any other
 	// async-emitted) outbound messages to the dashboard's SSE
 	// subscribers so the user sees the agent's reply live instead of
@@ -604,15 +632,19 @@ func (g *Gateway) Run() error {
 			case <-ctx.Done():
 				return
 			case <-reloadCh:
-				slog.Info("received reload signal, reloading agents")
+				slog.Info("received reload signal, reloading agents and scheduled jobs")
 				if err := g.ReloadAgents(); err != nil {
 					slog.Warn("agent reload failed", "error", err)
 				}
+				cron.NotifyJobCreated()
 			}
 		}
 	}()
 
 	var wg sync.WaitGroup
+	if err := g.bus.Start(ctx); err != nil {
+		return fmt.Errorf("start message bus: %w", err)
+	}
 	wg.Add(1)
 	go func() { defer wg.Done(); g.users.startEvictor(ctx) }()
 	wg.Add(1)
@@ -697,6 +729,14 @@ func makeStoreFirstAgentFileLoader(st store.Store) func(string, string) (config.
 func defaultStr(v, fallback string) string {
 	if v == "" {
 		return fallback
+	}
+	return v
+}
+
+func redisPrefix(v string) string {
+	v = strings.Trim(v, ":")
+	if v == "" {
+		return "fastagent"
 	}
 	return v
 }
@@ -815,7 +855,7 @@ func registerChannelsFromStore(st store.Store, mb *bus.MessageBus, chanMgr *chan
 }
 
 // allChannelRows returns every channel row regardless of ownership —
-// system rows ('','') plus per-user, per-agent, and per-(user, agent)
+// system rows ("", "") plus per-user, per-agent, and per-(user, agent)
 // rows. The boot path needs the union so each owner's adapter is
 // hot-started; per-row routing is decided later at message-receipt
 // time via LookupChannelByCredential.

internal/rediscoord/lease.go
@@ -1,0 +1,82 @@
+package rediscoord
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// Leaser implements channels.Leaser with Redis keys. Each lease is a single
+// key whose value is the current gateway holder ID and whose TTL is the
+// freshness window.
+type Leaser struct {
+	client *redis.Client
+	prefix string
+}
+
+func NewLeaser(client *redis.Client, prefix string) *Leaser {
+	prefix = strings.Trim(prefix, ":")
+	if prefix == "" {
+		prefix = "fastagent"
+	}
+	return &Leaser{client: client, prefix: prefix}
+}
+
+func (l *Leaser) Acquire(ctx context.Context, channel, accountID, holderID string, ttl time.Duration) (bool, error) {
+	key := l.key(channel, accountID)
+	ok, err := l.client.SetNX(ctx, key, holderID, ttl).Result()
+	if err != nil {
+		return false, err
+	}
+	if ok {
+		return true, nil
+	}
+	current, err := l.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	if current != holderID {
+		return false, nil
+	}
+	return l.Renew(ctx, channel, accountID, holderID, ttl)
+}
+
+func (l *Leaser) Renew(ctx context.Context, channel, accountID, holderID string, ttl time.Duration) (bool, error) {
+	res, err := renewScript.Run(ctx, l.client, []string{l.key(channel, accountID)}, holderID, int64(ttl/time.Millisecond)).Int()
+	if err != nil {
+		return false, err
+	}
+	return res == 1, nil
+}
+
+func (l *Leaser) Release(ctx context.Context, channel, accountID, holderID string) error {
+	_, err := releaseScript.Run(ctx, l.client, []string{l.key(channel, accountID)}, holderID).Int()
+	return err
+}
+
+func (l *Leaser) key(channel, accountID string) string {
+	if accountID == "" {
+		accountID = "default"
+	}
+	return fmt.Sprintf("%s:lease:channel:%s:%s", l.prefix, channel, accountID)
+}
+
+var renewScript = redis.NewScript(`
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+  return redis.call("PEXPIRE", KEYS[1], ARGV[2])
+end
+return 0
+`)
+
+var releaseScript = redis.NewScript(`
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+  return redis.call("DEL", KEYS[1])
+end
+return 0
+`)